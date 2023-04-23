3bde3a6e69444196ddc806eb2da29c195df0d6e2
EdwardGomes <edugomes514@gmail.com>
2023-04-23 19:04:08 -0300

    a

---
+++ lista-de-tarefas/index.js
@@ -0,0 +1,16 @@
+// adicionando a lógica da lista de tarefas//
+const listaDeTarefas = document.querySelector('#todo-list');
+const botaoAdicionar = document.querySelector('#add-btn');
+const inputTarefa = document.querySelector('#Adicionar-tarefa');
+const botaoApagar = document.querySelector('#delete-btn');
+const botaoApagarTudo = document.querySelector('#clear-btn');
+const marcarTudo = document.querySelector('#complete-all-btn');
+
+// Função para adicionar tarefas//
+
+function adicionarTarefa() {
+    
+}
+
+
+
